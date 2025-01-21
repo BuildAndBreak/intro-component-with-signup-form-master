6f2fa6f0cb3bb9ed44d8e7b48408cbb10356a993
BuildAndBreak <tiagodlapereira@gmail.com>
2025-01-21 19:55:26 +0100

    update js

---
+++ index.js
@@ -0,0 +1,75 @@
+const form = document.getElementById("form");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const tryParagraph = document.getElementById("try-it-paragraph");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); /*prevents the form from submitting in the default way*/
+  checkInputs();
+});
+
+tryParagraph.addEventListener("click", () => {
+  document.getElementById("submit").focus();
+});
+
+function checkInputs() {
+  //get the values from the inputs
+  const firstNameValue =
+    firstName.value.trim(); /* trim method is used to remove any spaces*/
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  let hasError = false;
+  const uppercaseRegex = /[A-Z]/;
+  const symbolRegex = /[!@#$%^&*(),.?":{}|<>]/;
+
+  if (firstNameValue === "") {
+    setErrorFor(firstName, "First name cannot be empty!");
+    hasError = true;
+  }
+  if (lastNameValue === "") {
+    setErrorFor(lastName, "Last name cannot be empty!");
+    hasError = true;
+  }
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be empty!");
+    hasError = true;
+  }
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be empty!");
+    hasError = true;
+  }
+  if (!uppercaseRegex.test(passwordValue)) {
+    setErrorFor(password, "Password must have at least one upper Case!");
+    hasError = true;
+  }
+  if (!symbolRegex.test(passwordValue)) {
+    setErrorFor(password, "Password must have at least one symbol!");
+    hasError = true;
+  }
+  if (passwordValue.length < 6) {
+    setErrorFor(password, "Password must have at least 6 caracters!");
+    hasError = true;
+  }
+  if (hasError) return;
+}
+
+function setErrorFor(input, message) {
+  const inputControl = input.parentElement;
+  const small = document.createElement("small");
+  small.classList.add("message");
+  small.innerHTML = message;
+  inputControl.appendChild(small);
+
+  input.addEventListener("input", () => {
+    clearError(input);
+  });
+}
+
+function clearError(input) {
+  const inputControl = input.closest(".input-control");
+  const small = inputControl.querySelector("small");
+  small.innerHTML = "";
+}
